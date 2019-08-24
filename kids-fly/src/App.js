04b7fcc3c3c30405666c62db1e4fba3ddac2f5e7
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-08-23 20:41:56 -0500

    trip info

--- kids-fly/src/App.js
+++ kids-fly/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import express from "express";
+import setGeneraliddleware from "./middleware/generalMiddleware";
+import errorHandler from "./middleware/errorHandler";
+import verifyToken from "./middleware/verifyToken";
+import * as user from "./controllers/user";
+import * as trip from "./controllers/trip";
+import * as login from "./controllers/login";
+import path from "path";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const server = express();
+setGeneraliddleware(server);
 
-export default App;
+server.use(express.static(path.resolve(path.join(__dirname, "../public"))));
+server.get("/", (__, res) => res.sendFile("index.html"));
+
+//login
+server.route("/login").post(login.login);
+
+server
+  .route("/users")
+  .get(verifyToken, user.getGeneral)
+  .post(user.postNew);
+
+//Auth required beyond this line
+server.use(verifyToken);
+
+server
+  .route("/trips")
+  .get(trip.getGeneral)
+  .post(trip.postNew);
+
+server
+  .route("/users/:id")
+  .get(user.getGeneral)
+  .put(user.put)
+  .delete(user.deleteU);
+
+server
+  .route("/trips/:id")
+  .get(trip.getGeneral)
+  .put(trip.put)
+  .delete(trip.deleteT);
+
+// server.use(errorHandler);
+
+export default server;
