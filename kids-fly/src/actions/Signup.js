4a324bb8c8c269187d15274c3687ad6dbdbb4bcb
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-08-29 20:51:42 -0500

    trip Info

---
+++ kids-fly/src/actions/Signup.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { SIGNUP_START, SIGNUP_SUCCESS, SIGNUP_FAILURE } from './index';
+
+export const addUser = user => dispatch => {
+  console.log('addUser ', user);
+  dispatch({ type: SIGNUP_START });
+  return axios
+    .post('https://kidsflyapi.herokuapp.com/auth/register', user)
+    .then(res => {
+      console.log(res);
+
+      dispatch({ type: SIGNUP_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: SIGNUP_FAILURE, payload: err.data });
+    });
+};
